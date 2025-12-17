571f1b53e80996584c4d534c45925fafbbad8f8b
Adnan Mohamed Khalid <adnanm281999@gmail.com>
2025-12-17 10:54:32 +0100

    Login page

--- src/pages/login/loginPage.js
+++ src/pages/login/loginPage.js
@@ -100,92 +100,96 @@ function Login() {
   };
 
   return (
-    <div id="main-wrapper" className="container">
-      <div className="row justify-content-center">
-        <div className="col-xl-12">
-          <div className="card border-0">
-            <div className="card-body p-0">
-              <div className="row no-gutters">
-                <div className="col-lg-6">
-                  <div className="p-4">
-                    <h3
-                      className="h5 mb-2"
-                      style={{ fontSize: "30px", fontFamily: "Roboto" }}
-                    >
-                      Welcome back to EM-Urgency
-                    </h3>
-                    <br />
-                    <div className="mb-4">
-                      <h1
-                        className="h4 font-weight-bold text-theme mb-4"
-                        style={{
-                          marginTop: "5px",
-                          fontSize: "60px",
-                          fontFamily: "Roboto",
-                          color: "#2C2424",
-                        }}
+    <div id="main-wrapper" className="min-vh-99 d-flex align-items-center justify-content-center">
+      <div className="container">
+        <div className="row justify-content-center w-100">
+          <div className="col-xl-12">
+            <div className="card border-0">
+              <div className="card-body p-0">
+                <div className="row no-gutters">
+                  <div className="col-lg-6">
+                    <div className="p-4 loginContent">
+                      <br />
+                      <br />
+                      <br />
+                      <br />
+                      <h3
+                        className="h5 mb-2"
+                        style={{ fontSize: "30px", fontFamily: "Roboto" }}
                       >
-                        Login
-                      </h1>
-                    </div>
-                    <form onSubmit={handleSubmit}>
-                      <div className="form-group">
-                        <input
-                          type="text"
-                          name="username"
-                          placeholder="Username"
-                          onChange={handleChange}
-                          className="userTextFeild"
-                        />
-                      </div>
-                      <br />
+                        Welcome back
+                      </h3>
 
-                      <div className="form-group">
-                        <input
-                          type="password"
-                          name="password"
-                          placeholder="Password"
-                          onChange={handleChange}
-                          className="password"
-                          id="exampleInputPassword1"
-                        />
-                      </div>
-                      <br />
-                      <button type="submit" className="btntheme">
-                        Login
-                      </button>
-                      <br />
-                      <br />
-                      <p style={{ color: "#E31836", textAlign: "center" }}>
-                        OR
-                      </p>
-                    </form>
-                    <GoogleLogin
-                      clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
-                      buttonText="Log in with Google"
-                      render={(renderProps) => (
-                        <button
-                          onClick={renderProps.onClick}
-                          className="signIn"
+                      <div className="mb-4">
+                        <h1
+                          className="h4 font-weight-bold text-theme mb-4"
+                          style={{
+                            marginTop: "5px",
+                            fontSize: "60px",
+                            fontFamily: "Roboto",
+                            color: "#2C2424",
+                          }}
                         >
-                          <img
-                            className="microsoftLogo"
-                            src={GoogleLogo}
-                            alt="logo"
-                          ></img>{" "}
-                          Sign In
+                          Login
+                        </h1>
+                      </div>
+                      <form onSubmit={handleSubmit}>
+                        <div className="form-group">
+                          <input
+                            type="text"
+                            name="username"
+                            placeholder="Username"
+                            onChange={handleChange}
+                            className="userTextFeild"
+                          />
+                        </div>
+                        <br />
+
+                        <div className="form-group">
+                          <input
+                            type="password"
+                            name="password"
+                            placeholder="Password"
+                            onChange={handleChange}
+                            className="password"
+                            id="exampleInputPassword1"
+                          />
+                        </div>
+                        <br />
+                        <button type="submit" className="btntheme">
+                          Login
                         </button>
-                      )}
-                      onSuccess={handleLogin}
-                      onFailure={handleFailure}
-                      cookiePolicy={"single_host_origin"}
-                    ></GoogleLogin>
+
+                        <br />
+                        <p className="orDivider">OR</p>
+                      </form>
+                      <GoogleLogin
+                        clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+                        buttonText="Log in with Google"
+                        render={(renderProps) => (
+                          <button
+                            onClick={renderProps.onClick}
+                            className="signIn"
+                          >
+                            <img
+                              className="microsoftLogo"
+                              src={GoogleLogo}
+                              alt="logo"
+                            ></img>{" "}
+                            Sign In
+                          </button>
+                        )}
+                        onSuccess={handleLogin}
+                        onFailure={handleFailure}
+                        cookiePolicy={"single_host_origin"}
+                      ></GoogleLogin>
+                    </div>
                   </div>
-                </div>
-                <div className="col-lg-6 d-none d-lg-inline-block">
-                  <div className="account-block rounded-right">
-                    <div className="overlay rounded-right" />
-                    <div className="account-testimonial">
+                  <div className="col-lg-6 d-none d-lg-inline-block">
+                    <div className="account-block rounded-right">
+                      <div className="overlay rounded-right" />
+                      <div className="account-testimonial">
+                      </div>
                     </div>
                   </div>
                 </div>
